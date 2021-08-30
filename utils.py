da7abcb96037573850a29eceeb810e2cbdd7f87c
IliaKhr <ikhrenkov@gmail.com>
2021-08-30 21:26:09 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,3 +12,18 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 logger.propagate = False
 logger.info('Logger настроен и работает')
+
+
+def fast_df_apply(df, func):
+    """
+    df <- датафрейм (срез колонок), к которому применяется функция
+    func <- python callableы, функция, которую необходимо применить к датафрейму
+    """
+    cols = list(df.columns)
+    data, index = [], []
+    for row in df.itertuples(index=True):
+        row_dict = {f:v for f,v in zip(cols, row[1:])}
+        data.append(func(row_dict))
+        index.append(row[0])
+
+    return pd.Series(data, index=index)
